1519a78ce20b54fc156d65f7c1d8c32395e27e65
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2023-02-25 18:39:20 +0100

    support for getting api client id/secret from environment in tests

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,11 @@
+import os
+
 CLIENT_ID = ''
 CLIENT_SECRET = ''
+
+if 'FFLOGSAPI_CID' in os.environ and 'FFLOGSAPI_SECRET' in os.environ:
+    CLIENT_ID = os.environ['FFLOGSAPI_CID']
+    CLIENT_SECRET = os.environ['FFLOGSAPI_SECRET']
 
 # 7 days
 CACHE_EXPIRY = 86400 * 7

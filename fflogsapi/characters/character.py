f3b0e7343a832a80cc753867419a4272169a4bde
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2023-02-26 20:03:08 +0100

    fix circular import between the region and character modules

--- fflogsapi/characters/character.py
+++ fflogsapi/characters/character.py
@@ -1,7 +1,5 @@
 from typing import TYPE_CHECKING, Any, Dict
 
-from ..world.region import FFLogsServer
-
 from ..util.indexing import itindex
 from ..util.decorators import fetch_data
 from ..util.filters import construct_filter_string
@@ -9,6 +7,7 @@ from .queries import Q_CHARACTER_DATA
 
 if TYPE_CHECKING:
     from ..client import FFLogsClient
+    from ..world.region import FFLogsServer
 
 
 class FFLogsCharacter:
@@ -81,6 +80,7 @@ class FFLogsCharacter:
         Returns:
             The character's server.
         '''
+        from ..world.region import FFLogsServer
         server_id = itindex(self._query_data('server{ id }'), self.DATA_INDICES)['server']['id']
         return FFLogsServer(filters={'id': server_id}, client=self._client)
 
